c0ffbdb1e900e7f2ac4b0f375640d1d8b4d84db7
Mirko Cukich <mirkocukich@gmail.com>
2019-10-01 21:54:41 -0500

    Assets linked to ViewController

--- UIDate Picker View/ViewController.swift
+++ UIDate Picker View/ViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbl1Label: UILabel!
+    @IBOutlet weak var lbl2Label: UILabel!
+    @IBOutlet weak var lbl3Label: UILabel!
+    @IBOutlet weak var lbl4Label: UILabel!
+    @IBOutlet weak var lbl5Label: UILabel!
+    @IBOutlet weak var lbl6Label: UILabel!
+    @IBOutlet weak var lbl7Label: UILabel!
+    @IBOutlet weak var lbl8Label: UILabel!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     
     }
 
-
+    @IBAction func datePickerUnit(_ sender: Any) {
+        
+        
+    }
+    
+    
 }
 
